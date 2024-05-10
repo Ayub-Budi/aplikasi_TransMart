02721e63568f4adc6b7a0f50fbcdeb2899f89a29
yubzz09 <ayubbudie02@gmail.com>
2024-05-10 13:58:59 +0700

    memperbaiki menu Tarik Barang

--- app/src/main/java/com/a213310009ayubbudisantoso/transmart/BebasExpiredFragment.kt
+++ app/src/main/java/com/a213310009ayubbudisantoso/transmart/BebasExpiredFragment.kt
@@ -1,7 +1,9 @@
 package com.a213310009ayubbudisantoso.transmart
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,15 +11,23 @@ import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.a213310009ayubbudisantoso.transmart.api.model.TarikBarangModel
+import com.a213310009ayubbudisantoso.transmart.api.services.TarikBarangService
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class BebasExpiredFragment : Fragment() {
     private lateinit var kembali: ImageView
     private lateinit var pieChart: PieChart
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +45,9 @@ class BebasExpiredFragment : Fragment() {
         val radiusA = 10f // Variabel untuk radius
         val radiusB = 2f // Variabel untuk radius
 
-        drawPieChart(listOf(radiusA, radiusB), listOf(Color.BLUE, Color.RED))
+        drawPieChart(listOf(radiusA, radiusB), listOf(Color.BLUE, Color.GREEN))
+
+        fetchDataFromAPIListExpired()
 
         dataExpired.setOnClickListener {
             findNavController().navigate(R.id.action_bebasExpiredFragment_to_dataExpiredFragment2)
@@ -63,4 +75,51 @@ class BebasExpiredFragment : Fragment() {
         pieChart.data = data
         pieChart.invalidate()
     }
+
+    private fun fetchDataFromAPIListExpired() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://backend.transmart.co.id/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(TarikBarangService::class.java)
+
+        apiService.getListExpired().enqueue(object : Callback<List<TarikBarangModel>> {
+            override fun onResponse(call: Call<List<TarikBarangModel>>, response: Response<List<TarikBarangModel>>) {
+                if (response.isSuccessful) {
+                    val itemList = response.body()
+                    itemList?.let {
+                        saveDataToSharedPreferences(requireContext(), it)
+                    }
+
+                } else {
+                    // Tangani kesalahan jika respons tidak berhasil
+                    Log.e("TarikBarangFragment", "Gagal mendapatkan data: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<TarikBarangModel>>, t: Throwable) {
+                // Tangani kesalahan koneksi atau respons gagal
+                Log.e("TarikBarangFragment", "Error: ${t.message}")
+            }
+        })
+    }
+
+    fun saveDataToSharedPreferences(context: Context, itemList: List<TarikBarangModel>) {
+        val sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(itemList)
+        editor.putString("listDataExpired", json)
+        editor.apply()
+    }
+
+//    fun loadDataFromSharedPreferences(context: Context): List<TarikBarangModel>? {
+//        val sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+//        val gson = Gson()
+//        val json = sharedPreferences.getString("listDataExpired", null)
+//        val type = object : TypeToken<List<TarikBarangModel>>() {}.type
+//        return gson.fromJson(json, type)
+//    }
+
 }
--- app/src/main/java/com/a213310009ayubbudisantoso/transmart/TarikBarangFragment.kt
+++ app/src/main/java/com/a213310009ayubbudisantoso/transmart/TarikBarangFragment.kt
@@ -1,5 +1,6 @@
 package com.a213310009ayubbudisantoso.transmart
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -14,6 +15,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.a213310009ayubbudisantoso.transmart.api.adapter.TarikBarangAdapter
 import com.a213310009ayubbudisantoso.transmart.api.model.TarikBarangModel
 import com.a213310009ayubbudisantoso.transmart.api.services.TarikBarangService
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -55,39 +58,81 @@ class TarikBarangFragment : Fragment() {
 
         recyclerView.adapter = adapter
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://backend.transmart.co.id/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        displaySavedResponse()
+        // Load data dari penyimpanan lokal
+//        val itemList = loadDataFromSharedPreferences(requireContext())
+//        itemList?.let {
+//            adapter.setData(it)
+//        }
 
-        var apiService = retrofit.create(TarikBarangService::class.java)
+//        fetchDataFromAPI()
 
-        apiService.getListExpired().enqueue(object : Callback<List<TarikBarangModel>> {
-            override fun onResponse(call: Call<List<TarikBarangModel>>, response: Response<List<TarikBarangModel>>) {
-                if (response.isSuccessful) {
-                    val itemList = response.body()
-//                    itemList?.forEach { item ->
-//                        Log.d("Response", item.toString())
-//                    }
-
-                    itemList?.let {
-                        adapter.setData(it)
-                    }
-
-                } else {
-                    // Tangani kesalahan jika respons tidak berhasil
-                    Log.e("TarikBarangFragment", "Gagal mendapatkan data: ${response.message()}")
-                }
-            }
-
-            override fun onFailure(call: Call<List<TarikBarangModel>>, t: Throwable) {
-                // Tangani kesalahan koneksi atau respons gagal
-                Log.e("TarikBarangFragment", "Error: ${t.message}")
-            }
-        })
     }
 
 
 
+    private fun displaySavedResponse() {
+        val sharedPreferences = requireContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val responseJson = sharedPreferences.getString("listDataExpired", "")
+
+        // Mengonversi JSON string kembali menjadi objek List<TarikBarangModel>
+        val itemType = object : TypeToken<List<TarikBarangModel>>() {}.type
+        val itemList: List<TarikBarangModel> = Gson().fromJson(responseJson, itemType)
+
+        // Menampilkan data di RecyclerView jika ada
+        if (itemList.isNotEmpty()) {
+            adapter.setData(itemList)
+        } else {
+            // Tangani kasus di mana tidak ada data yang tersimpan
+            Log.d("displaySavedResponse", "Tidak ada data yang tersimpan")
+        }
+    }
+
+//    private fun fetchDataFromAPI() {
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("https://backend.transmart.co.id/")
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//
+//        val apiService = retrofit.create(TarikBarangService::class.java)
+//
+//        apiService.getListExpired().enqueue(object : Callback<List<TarikBarangModel>> {
+//            override fun onResponse(call: Call<List<TarikBarangModel>>, response: Response<List<TarikBarangModel>>) {
+//                if (response.isSuccessful) {
+//                    val itemList = response.body()
+//                    itemList?.let {
+//                        adapter.setData(it)
+//                        saveDataToSharedPreferences(requireContext(), it)
+//                    }
+//
+//                } else {
+//                    // Tangani kesalahan jika respons tidak berhasil
+//                    Log.e("TarikBarangFragment", "Gagal mendapatkan data: ${response.message()}")
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<TarikBarangModel>>, t: Throwable) {
+//                // Tangani kesalahan koneksi atau respons gagal
+//                Log.e("TarikBarangFragment", "Error: ${t.message}")
+//            }
+//        })
+//    }
+
+//    fun saveDataToSharedPreferences(context: Context, itemList: List<TarikBarangModel>) {
+//        val sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//        val gson = Gson()
+//        val json = gson.toJson(itemList)
+//        editor.putString("listDataExpired", json)
+//        editor.apply()
+//    }
+//
+//    fun loadDataFromSharedPreferences(context: Context): List<TarikBarangModel>? {
+//        val sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+//        val gson = Gson()
+//        val json = sharedPreferences.getString("listDataExpired", null)
+//        val type = object : TypeToken<List<TarikBarangModel>>() {}.type
+//        return gson.fromJson(json, type)
+//    }
 
 }
